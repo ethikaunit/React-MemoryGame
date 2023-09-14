ad428f731a8027f97f7fc82d27134e7d79d0460b
ETHIKA-UNIT <136907485+ethikaunit@users.noreply.github.com>
2023-09-14 08:46:05 +0530

    Update card.jsx
    all images are imported in card.jsx

--- src/component/card.jsx
+++ src/component/card.jsx
@@ -1,4 +1,13 @@
 import React from 'react'
+import black './imgs/black.png'
+import blue './imgs/blue.png'
+import cyan './imgs/cyan.png'
+import orange './imgs/orange.png'
+import purple './imgs/purple.png'
+import red './imgs/red.png'
+import white './imgs/white.png'
+import yellow './imgs/yellow.png'
+
 
 function card({index, card, flip}) {
     
@@ -9,7 +18,7 @@ function card({index, card, flip}) {
              onClick={()=>flip(index)}
             
              >
-            <img src={'./imgs/'+card.img+'.png'}></img>
+            <img src={card.img}></img>
            
            
         </div>
